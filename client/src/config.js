a78c2095d00fe679bcdb549e835eef78d972acd0
Sathya Venugopal <sathyavenugopal@Sathyas-MacBook-Pro.local>
2023-06-26 20:19:04 -0700

    minor typo

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const backendUrl = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:4000' : 'https://nba-stats-app-backend.onrender.com';
+export const backendUrl = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:4000' : 'https://nba-stats-app-backend.onrender.com';
